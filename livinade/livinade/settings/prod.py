1627684549e47183a34e75d6994d7cf88467ff77
Allen <awhearry@gmail.com>
2016-09-29 20:41:47 -0400

    edit production settings

--- livinade/livinade/settings/prod.py
+++ livinade/livinade/settings/prod.py
@@ -21,6 +21,7 @@ INSTALLED_APPS += [
 ]
 
 # configure amazon web services
+AWS_QUERYSTRING_AUTH = False
 AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
 AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
 DEFAULT_FILE_STORAGE = "livinade.utils.MediaRootS3BotoStorage"
